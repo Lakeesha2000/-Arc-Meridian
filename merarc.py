ef7d81ab3531f00aaf2cc8e0605ac99384c53136
Lakeesha <95014356+Lakeesha2000@users.noreply.github.com>
2021-11-25 18:06:19 +0530

    Update merarc.py

--- merarc.py
+++ merarc.py
@@ -1,20 +1,20 @@
+import math
 def merarc(a,equad,lat1,lat2):
-    b=a*sqrt(1-equad)
+    b=a*math.sqrt(1-equad)
     n=(a-b)/(a+b)
     a0=1.+((n**2)/4.)+((n**4)/64.)
     a2=(3./2.)*(n-((n**3)/8.))
     a4=(15./16.)*((n**2)-((n**4)/4.))
     a6=(35./48.)*(n**3)
 
-    s1=a/(1+n)*(a0*lat1-a2*sin(2.*lat1)+a4*sin(4.*lat1)-a6*sin(6.*lat1))
-    s2=a/(1+n)*(a0*lat2-a2*sin(2.*lat2)+a4*sin(4.*lat2)-a6*sin(6.*lat2))
+    s1=a/(1+n)*(a0*lat1-a2*math.sin(2.*lat1)+a4*math.sin(4.*lat1)-a6*math.sin(6.*lat1))
+    s2=a/(1+n)*(a0*lat2-a2*math.sin(2.*lat2)+a4*math.sin(4.*lat2)-a6*math.sin(6.*lat2))
     return s2-s1
     
 a = 6378137.
 equad =0.00669437999
 lat=53.5
 lat0=53.5
-lat0=radians(lat0)
-y=arcmer(a,equad,lat0,lat)
-print y
- 
+lat0=math.radians(lat0)
+y=merarc(a,equad,lat0,lat)
+print(y)
